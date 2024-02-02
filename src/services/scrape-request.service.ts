094442a9eb48c38cada92a1fb86e228694f5c70f
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-02-02 21:51:28 +0700

    Remove unnecesarry api on scrape request service

--- src/services/scrape-request.service.ts
+++ src/services/scrape-request.service.ts
@@ -37,32 +37,6 @@ const postScrapeRequests = ({
   });
 };
 
-const register = (username: string, email: string, password: string) => {
-  return axios.post(API_PATH + "/signup", {
-    username,
-    email,
-    password,
-  });
-};
-
-const login = async (username: string, password: string) => {
-  console.log("API_URL: ", API_PATH)
-  const response = await axios
-    .post(API_URL + "/signin", {
-      username,
-      password,
-    });
-  console.log(response.data);
-  if (response.data.accessToken) {
-    localStorage.setItem("user", JSON.stringify(response.data));
-  }
-  return response.data;
-};
-
-const logout = () => {
-  localStorage.removeItem("user");
-};
-
 const scrapeRequestService = {
   fetchScrapeRequests,
   postScrapeRequests
