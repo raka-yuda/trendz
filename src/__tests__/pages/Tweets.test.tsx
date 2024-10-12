fb4fc22773beaab7741fd5079689496f77ed3bcd
Raka Yuda Pradipta <raka.yuda.pradipta@gmail.com>
2024-10-12 23:53:16 +0700

    feat: remove unnecesary line

--- src/__tests__/pages/Tweets.test.tsx
+++ src/__tests__/pages/Tweets.test.tsx
@@ -4,18 +4,15 @@ import { Provider } from 'react-redux';
 import store from '../../store';
 import App from '../../App';
 import { describe, it, expect, vi } from 'vitest';
-import { fetchTweetsChartDashboardData } from '../../actions/tweets';
 
 // Mock the Redux hooks and Provider
 vi.mock('react-redux', () => ({
   useSelector: () => ({
     user: { roles: ['ROLE_USER'] },
   }),
-  // useDispatch: () => vi.fn(),
   useDispatch: () => () => Promise.resolve([
     { type: 'count_success_topic_scrape', count: '100' },
     { type: 'count_sentiment_scrape', count: '50' },
-    // Add more mock data here
   ]),
   Provider: ({ children }: { children: React.ReactNode }) => <>{children}</>
 }));
@@ -37,16 +34,16 @@ vi.mock('../common/Loader', () => ({
 // Mock the fetchTweetsChartDashboardData action
 vi.mock('../actions/tweets', () => ({
   fetchTweetsChartDashboardData: () => Promise.resolve([
-    // { type: 'count_success_topic_scrape', count: '100' },
-    // { type: 'count_sentiment_scrape', count: '50' },
+    { type: 'count_success_topic_scrape', count: '100' },
+    { type: 'count_sentiment_scrape', count: '50' },
   ]),
   fetchTweetsChartSentiment: () => Promise.resolve([
-    // { type: 'count_success_topic_scrape', count: '100' },
-    // { type: 'count_sentiment_scrape', count: '50' },
+    { type: 'count_success_topic_scrape', count: '100' },
+    { type: 'count_sentiment_scrape', count: '50' },
   ]),
   fetchTweetsChartScrapeSentiment: () => Promise.resolve([
-    // { type: 'count_success_topic_scrape', count: '100' },
-    // { type: 'count_sentiment_scrape', count: '50' },
+    { type: 'count_success_topic_scrape', count: '100' },
+    { type: 'count_sentiment_scrape', count: '50' },
   ]),
 }));
 
@@ -64,7 +61,6 @@ describe('App Component', () => {
       </MemoryRouter>
     );
 
-    // Explicitly wait for the same duration as set in your App component
     await waitFor(() => {
       expect(screen.getByText(/login/i, { selector: 'h3' })).toBeInTheDocument();
     }, { timeout: 2000 });
@@ -79,7 +75,6 @@ describe('App Component', () => {
       </MemoryRouter>
     );
 
-    // Explicitly wait for the same duration as set in your App component
     await waitFor(() => {
       expect(screen.getByTestId("pagename")).toHaveTextContent('Tweets');
     }, { timeout: 2000 });
